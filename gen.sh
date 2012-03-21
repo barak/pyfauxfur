6c100829c663821028610cc0a643e3a70a12ec6f
Barak A. Pearlmutter <barak+git@cs.nuim.ie>
2012-03-21 13:29:46 +0000

    mention python-dev; migrate some Debian instructions to code in gen.sh

--- gen.sh
+++ gen.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-PYUIC=`which pyuic4`
-
-if [ ! -x "$PYUIC" ]; then 
-	PYUIC="/opt/local/Library/Frameworks/Python.framework/Versions/2.5/bin/pyuic4";	
+if type -p pyuic4 > /dev/null; then
+    PYUIC=pyuic4
+elif type -p pyuic > /dev/null; then
+    PYUIC=pyuic
+elif [ -f /usr/share/pyshared/PyQt4/uic/pyuic.py ]; then
+    PYUIC="python /usr/share/pyshared/PyQt4/uic/pyuic.py"
+elif [ -x /opt/local/Library/Frameworks/Python.framework/Versions/2.5/bin/pyuic4 ]; then 
+    PYUIC=/opt/local/Library/Frameworks/Python.framework/Versions/2.5/bin/pyuic4
+else
+    echo "error: cannot find pyuic"
+    exit 1
 fi
+
+echo "using \"$PYUIC\""
 	
 $PYUIC MainWindow.ui > Ui_MainWindow.py
